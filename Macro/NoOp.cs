f978aec7c930f2a0e0acdccea726e264b2f49d39
gamli <unknown>
2014-12-02 21:13:45 +0100

    Added missing files

---
+++ Macro/NoOp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macro
+{
+   public class NoOp : Base
+   {
+      public override void Accept(IVisitor Visitor)
+      {
+         Visitor.VisitNoOp(this);
+      }
+   }
+}
---
+++ Macro_TEST/Click_TEST.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Macro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Macro_TEST
+{
+   [TestClass]
+   public class Click_TEST
+   {
+      [TestMethod]
+      public void Accept_TEST()
+      {
+         var click = new LeftClick();
+         var testVisitor = new TestVisitor();
+         click.Accept(testVisitor);
+         Assert.AreEqual(testVisitor.Macros.Count, 1);
+         Assert.AreEqual(testVisitor.Macros[0].Macro.GetType(), typeof(LeftClick));
+      }
+   }
+}
