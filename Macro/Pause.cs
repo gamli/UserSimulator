d90eb3aac0558709ffe909ce10d2cd6e929f255d
gamli <unknown>
2014-12-03 22:49:24 +0100

    Excluded Duration from code coverage

--- Macro/Pause.cs
+++ Macro/Pause.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Macro
    public class Pause : MacroBase
    {
       private TimeSpan _duration;
+      [ExcludeFromCodeCoverage]
       public TimeSpan Duration { get { return _duration; } set { SetPropertyValue(ref _duration, value); } }
 
       public override void Accept(IVisitor Visitor)
