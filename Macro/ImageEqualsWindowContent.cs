05a49c1c4572b0596761a0695d23ce6a07f83869
gamli <unknown>
2014-12-03 22:47:55 +0100

    Excluded simple properties from code coverage

--- Macro/ImageEqualsWindowContent.cs
+++ Macro/ImageEqualsWindowContent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,19 @@ namespace Macro
    public class ImageEqualsWindowContent : MacroWithBodyBase
    {
       private Image _image;
+      [ExcludeFromCodeCoverage]
       public Image Image { get { return _image; } set { SetPropertyValue(ref _image, value); } }
 
       private IntPtr _window;
+      [ExcludeFromCodeCoverage]
       public IntPtr Window { get { return _window; } set { SetPropertyValue(ref _window, value); } }
 
       private int _positionX;
+      [ExcludeFromCodeCoverage]
       public int PositionX { get { return _positionX; } set { SetPropertyValue(ref _positionX, value); } }
 
       private int _positionY;
+      [ExcludeFromCodeCoverage]
       public int PositionY { get { return _positionY; } set { SetPropertyValue(ref _positionY, value); } }
 
       public override void Accept(IVisitor Visitor)
