773e0418f5f06069aae011056df05d6b3712b941
gamli <unknown>
2014-12-03 21:46:13 +0100

    Added test for DisposableBase

---
+++ Common_TEST/DisposableBase_TEST.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common_TEST
+{
+   [TestClass]
+   public class DisposableBase_TEST
+   {
+      [TestMethod]
+      public void Dispose_TEST()
+      {
+         var testObject = new TestDisposableBase();
+         Assert.AreEqual(testObject.DisposeCallCounter, 0);
+         testObject.Dispose();
+         Assert.AreEqual(testObject.DisposeCallCounter, 1);
+         testObject.Dispose();
+         Assert.AreEqual(testObject.DisposeCallCounter, 1);
+      }
+
+      private class TestDisposableBase : DisposableBase
+      {
+         public int DisposeCallCounter { get; private set; }
+
+         protected override void Dispose(bool Disposing)
+         {
+            if (Disposing)
+               DisposeCallCounter++;
+            base.Dispose(Disposing);
+         }
+      }
+   }
+}
