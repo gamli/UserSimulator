d23b3369782894c3dcfa47f18cc354871bad6c00
gamli <unknown>
2014-12-03 22:48:27 +0100

    Excluded Body from code coverage

--- Macro/MacroBase.cs
+++ Macro/MacroBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Macro
    public abstract class MacroWithBodyBase : MacroBase
    {
       private MacroBase _body;
+      [ExcludeFromCodeCoverage]
       public MacroBase Body { get { return _body; } set { SetPropertyValue(ref _body, value); } }
    }
    
