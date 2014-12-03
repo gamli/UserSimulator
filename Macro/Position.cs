c86c8d8f4c6516a98055f68492af1b84377e03d7
gamli <unknown>
2014-12-03 22:49:50 +0100

    Excluded simple properties from code coverage

--- Macro/Position.cs
+++ Macro/Position.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace Macro
    public class Position : MacroBase
    {
       private int _x;
+      [ExcludeFromCodeCoverage]
       public int X { get { return _x; } set { SetPropertyValue(ref _x, value); } }
 
       private int _y;
+      [ExcludeFromCodeCoverage]
       public int Y { get { return _y; } set { SetPropertyValue(ref _y, value); } }
 
       public override void Accept(IVisitor Visitor)
