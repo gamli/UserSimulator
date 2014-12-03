68c425218fa9ecfe9fa33b4e1d0ee967b9a450e3
gamli <unknown>
2014-12-03 22:48:55 +0100

    Excluded simple properties from code coverage

--- Macro/Move.cs
+++ Macro/Move.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace Macro
    public class Move : MacroBase
    {
       private int _translationX;
+      [ExcludeFromCodeCoverage]
       public int TranslationX { get { return _translationX; } set { SetPropertyValue(ref _translationX, value); } }
 
       private int _translationY;
+      [ExcludeFromCodeCoverage]
       public int TranslationY { get { return _translationY; } set { SetPropertyValue(ref _translationY, value); } }
 
       public override void Accept(IVisitor Visitor)
