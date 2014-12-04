2febcc9b3e164e86655037c85a40e9605523cec4
gamli <unknown>
2014-12-04 15:08:06 +0100

    Added missing view model test

---
+++ MacroViewModel_TEST/MacroWithBodyBaseVM_TEST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Macro;
+using MacroViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MacroViewModel_TEST
+{
+   [TestClass]
+   public class MacroWithBodyBaseVM_TEST
+   {
+
+      [TestMethod]
+      public void Body_Property_TEST()
+      {
+         var mockMacro = new MockMacro();
+         using(var mockMacroVM = new MockMacroVM(mockMacro))
+         {
+            Assert.AreSame(mockMacro.Body, mockMacroVM.BodyVM.Model);
+         }
+      }
+
+      [ExcludeFromCodeCoverage]
+      private class MockMacroVM : MacroWithBodyBaseVM<MockMacro>
+      {
+         public MockMacroVM(MockMacro Model)
+            : base(Model)
+         {
+            // nothing to do
+         }
+      }
+
+      [ExcludeFromCodeCoverage]
+      private class MockMacro : MacroWithBodyBase
+      {
+         public MockMacro()
+         {
+            Body = new NoOp();
+         }
+
+         public override void Accept(IVisitor Visitor)
+         {
+            throw new NotImplementedException();
+         }
+      }
+   }
+}
