1e969efa5773ffd2b6361df31a46c1c9733f3d82
gamli <unknown>
2014-12-04 10:44:38 +0100

    Gave class its own file

---
+++ MacroViewModel/MacroWithBodyBaseVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Macro;
+
+namespace MacroViewModel
+{
+   public abstract class MacroWithBodyBaseVM<T> : MacroBaseVM<T>
+      where T : MacroWithBodyBase
+   {
+      private NotifyingTransformedProperty<MacroBaseVM> _bodyVM;
+      public MacroBaseVM BodyVM
+      {
+         get
+         {
+            return _bodyVM.Value;
+         }
+      }
+
+      protected MacroWithBodyBaseVM(T Model)
+         : base(Model)
+      {
+         _bodyVM =
+            new NotifyingTransformedProperty<MacroBaseVM>(
+               new[] { "Body" }, "BodyVM",
+               Model, this,
+               () => MacroViewModelFactory.Instance.Create(Model.Body),
+               VM => VM.Dispose());
+      }
+
+      protected override void Dispose(bool Disposing)
+      {
+         if (Disposing)
+            _bodyVM.Dispose();
+         base.Dispose(Disposing);
+      }
+   }
+}
