1b800296a87a8dde89b093f6dba9229b988b2fa1
gamli <unknown>
2015-01-07 19:00:59 +0100

    Missing file

---
+++ MacroRuntime/RuntimeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Macro;
+
+namespace MacroRuntime
+{
+   public class RuntimeException : Exception
+   {
+      public MacroBase Macro { get; set; }
+
+      public ContextBase Context { get; set; }
+
+      public RuntimeException(string Message, MacroBase Macro, ContextBase Context, Exception InnerException = null)
+         : base(Message, InnerException)
+      {
+         this.Macro = Macro;
+         this.Context = Context;
+      }
+   }
+}
